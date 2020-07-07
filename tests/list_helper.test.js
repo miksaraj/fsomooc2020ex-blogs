e2b4f4f619631b8213fabee89bc3beddfd810d09
Mikko Rajakangas <mikko.rajakangas@visma.com>
2020-07-08 00:48:05 +0300

    more tests

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -16,7 +16,7 @@ const listWithSeveralBlogs = [...listWithOneBlog, {
     title: 'How to run MongoDB - the Beginner\'s Guide',
     author: 'Joseph Kelley',
     url: 'www.mongodb4dummies.net',
-    likes: 535,
+    likes: 16,
     __v: 0
 },
 {
@@ -25,6 +25,46 @@ const listWithSeveralBlogs = [...listWithOneBlog, {
     author: 'Yannis K. Marg',
     url: 'www.dev.to/ykmarg/me-n-stack-holy-grail',
     likes: 11,
+    __v: 0
+}]
+const listWithMoreBlogs = [...listWithSeveralBlogs, {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+},
+{
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0 
+},
+{
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+},
+{
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+},
+{
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
     __v: 0
 }]
 
@@ -47,7 +87,7 @@ describe('total likes', () => {
 
     test('of a bigger list is calculated right', () => {
         const result = listHelper.totalLikes(listWithSeveralBlogs)
-        expect(result).toBe(551)
+        expect(result).toBe(32)
     })
 })
 
@@ -77,8 +117,56 @@ describe('favorite blog', () => {
             title: 'How to run MongoDB - the Beginner\'s Guide',
             author: 'Joseph Kelley',
             url: 'www.mongodb4dummies.net',
-            likes: 535,
+            likes: 16,
             __v: 0
+        })
+    })
+})
+
+describe('most blogs', () => {
+
+    test('of empty list is null', () => {
+        const result = listHelper.mostBlogs(emptyList)
+        expect(result).toEqual(null)
+    })
+
+    test('when list has only one blog equals given author and one blog', () => {
+        const result = listHelper.mostBlogs(listWithOneBlog)
+        expect(result).toEqual({
+            author: 'Edsger W. Dijkstra',
+            blogs: 1
+        })
+    })
+
+    test('of a bigger list is the author with most blogs', () => {
+        const result = listHelper.mostBlogs(listWithMoreBlogs)
+        expect(result).toEqual({
+            author: 'Robert C. Martin',
+            blogs: 3
+        })
+    })
+})
+
+describe('most likes', () => {
+
+    test('of empty list is null', () => {
+        const result = listHelper.mostLikes(emptyList)
+        expect(result).toEqual(null)
+    })
+
+    test('when list has only one blog equals given author and the likes of that', () => {
+        const result = listHelper.mostLikes(listWithOneBlog)
+        expect(result).toEqual({
+            author: 'Edsger W. Dijkstra',
+            likes: 5
+        })
+    })
+
+    test('of a bigger list is the author with most likes', () => {
+        const result = listHelper.mostLikes(listWithMoreBlogs)
+        expect(result).toEqual({
+            author: 'Edsger W. Dijkstra',
+            likes: 17
         })
     })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -16,8 +18,36 @@ const favoriteBlog = (blogs) => {
     return fav || null
 }
 
+const mostBlogs = (blogs) => {
+    blogsByAuthor = []
+    const authors = [...new Set(blogs.map(blog => blog.author))]
+    authors.forEach(author => {
+        const entries = blogs.filter(blog => blog.author === author).length
+        blogsByAuthor.push({author, blogs: entries})
+    })
+    const max = Math.max.apply(Math, blogsByAuthor.map(o => o.blogs))
+    return blogsByAuthor.find(x => x.blogs === max) || null
+}
+
+const mostLikes = (blogs) => {
+    likesByAuthor = []
+    const authors = [...new Set(blogs.map(blog => blog.author))]
+    authors.forEach(author => {
+        const entries = blogs.filter(blog => blog.author === author)
+        const likesByEntry = entries.map(entry => entry.likes)
+        const totalLikes = likesByEntry.reduce(function(a,b) {
+            return a + b
+        }, 0)
+        likesByAuthor.push({author, likes: totalLikes})
+    })
+    const max = Math.max.apply(Math, likesByAuthor.map(o => o.likes))
+    return likesByAuthor.find(x => x.likes === max) || null
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
